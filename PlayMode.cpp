59b27e26713eb9bf58d5b21cdf20b310026a67c1
Snoopy <2322168080@qq.com>
2020-10-31 17:06:01 -0400

    Fix build issue on linux and MacOS

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -100,17 +100,17 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			0.0f, 0.0f, 0.0f, 1.0f
 		));
 
-		auto draw_text = [&](glm::vec2 const &at, std::string const &text, float H) {
-			lines.draw_text(text,
-				glm::vec3(at.x, at.y, 0.0),
-				glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-				glm::u8vec4(0x00, 0x00, 0x00, 0x00));
-			float ofs = 2.0f / drawable_size.y;
-			lines.draw_text(text,
-				glm::vec3(at.x + ofs, at.y + ofs, 0.0),
-				glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-				glm::u8vec4(0xff, 0xff, 0xff, 0x00));
-		};
+		// auto draw_text = [&](glm::vec2 const &at, std::string const &text, float H) {
+		// 	lines.draw_text(text,
+		// 		glm::vec3(at.x, at.y, 0.0),
+		// 		glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+		// 		glm::u8vec4(0x00, 0x00, 0x00, 0x00));
+		// 	float ofs = 2.0f / drawable_size.y;
+		// 	lines.draw_text(text,
+		// 		glm::vec3(at.x + ofs, at.y + ofs, 0.0),
+		// 		glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+		// 		glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+		// };
 
 		// draw_text(glm::vec2(-aspect + 0.1f, 0.0f), server_message, 0.09f);
 
