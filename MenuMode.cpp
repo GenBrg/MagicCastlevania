55252602a345667e26fc26e4ec11a928649699c5
Chipxiang <jianxiangli1996@gmail.com>
2020-11-08 20:03:33 -0500

    Added dialog box, modified player jump conifg.

---
+++ MenuMode.cpp
@@ -0,0 +1,188 @@
+#include "MenuMode.hpp"
+
+//for the GL_ERRORS() macro:
+#include "gl_errors.hpp"
+
+//for easy sprite drawing:
+#include "DrawSprites.hpp"
+
+//for playing movement sounds:
+#include "Sound.hpp"
+
+//for loading:
+#include "Load.hpp"
+
+#include <random>
+
+Load< Sound::Sample > sound_click(LoadTagDefault, []() -> Sound::Sample* {
+	std::vector< float > data(size_t(48000 * 0.2f), 0.0f);
+	for (uint32_t i = 0; i < data.size(); ++i) {
+		float t = i / float(48000);
+		//phase-modulated sine wave (creates some metal-like sound):
+		data[i] = std::sin(3.1415926f * 2.0f * 440.0f * t + std::sin(3.1415926f * 2.0f * 450.0f * t));
+		//quadratic falloff:
+		data[i] *= 0.3f * std::pow(std::max(0.0f, (1.0f - t / 0.2f)), 2.0f);
+	}
+	return new Sound::Sample(data);
+	});
+
+Load< Sound::Sample > sound_clonk(LoadTagDefault, []() -> Sound::Sample* {
+	std::vector< float > data(size_t(48000 * 0.2f), 0.0f);
+	for (uint32_t i = 0; i < data.size(); ++i) {
+		float t = i / float(48000);
+		//phase-modulated sine wave (creates some metal-like sound):
+		data[i] = std::sin(3.1415926f * 2.0f * 220.0f * t + std::sin(3.1415926f * 2.0f * 200.0f * t));
+		//quadratic falloff:
+		data[i] *= 0.3f * std::pow(std::max(0.0f, (1.0f - t / 0.2f)), 2.0f);
+	}
+	return new Sound::Sample(data);
+	});
+
+
+MenuMode::MenuMode(std::vector< Item > const& items_) : items(items_) {
+
+	//select first item which can be selected:
+	for (uint32_t i = 0; i < items.size(); ++i) {
+		if (items[i].on_select) {
+			selected = i;
+			break;
+		}
+	}
+}
+
+MenuMode::~MenuMode() {
+}
+
+bool MenuMode::handle_event(SDL_Event const& evt, glm::uvec2 const& window_size) {
+	if (evt.type == SDL_KEYDOWN) {
+		if (evt.key.keysym.sym == SDLK_UP) {
+			//skip non-selectable items:
+			for (uint32_t i = selected - 1; i < items.size(); --i) {
+				if (items[i].on_select) {
+					selected = i;
+					Sound::play(*sound_click);
+					break;
+				}
+			}
+			return true;
+		}
+		else if (evt.key.keysym.sym == SDLK_DOWN) {
+			//note: skips non-selectable items:
+			for (uint32_t i = selected + 1; i < items.size(); ++i) {
+				if (items[i].on_select) {
+					selected = i;
+					Sound::play(*sound_click);
+					break;
+				}
+			}
+			return true;
+		}
+		else if (evt.key.keysym.sym == SDLK_RETURN) {
+			if (selected < items.size() && items[selected].on_select) {
+				Sound::play(*sound_clonk);
+				items[selected].on_select(items[selected]);
+				return true;
+			}
+		}
+	}
+	if (background) {
+		return background->handle_event(evt, window_size);
+	}
+	else {
+		return false;
+	}
+}
+
+void MenuMode::update(float elapsed) {
+
+	select_bounce_acc = select_bounce_acc + elapsed / 0.7f;
+	select_bounce_acc -= std::floor(select_bounce_acc);
+
+	if (background) {
+		background->update(elapsed);
+	}
+}
+
+void MenuMode::draw(glm::uvec2 const& drawable_size) {
+	if (background) {
+		std::shared_ptr< Mode > hold_me = shared_from_this();
+		background->draw(drawable_size);
+		//it is an error to remove the last reference to this object in background->draw():
+		assert(hold_me.use_count() > 1);
+	}
+	else {
+		glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	//use alpha blending:
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//don't use the depth test:
+	glDisable(GL_DEPTH_TEST);
+
+	float bounce = (0.25f - (select_bounce_acc - 0.5f) * (select_bounce_acc - 0.5f)) / 0.25f * select_bounce_amount;
+
+	{ //draw the menu using DrawSprites:
+		assert(atlas && "it is an error to try to draw a menu without an atlas");
+		DrawSprites draw_sprites(*atlas, view_min, view_max, drawable_size, DrawSprites::AlignPixelPerfect);
+
+		for (auto const& item : items) {
+			bool is_selected = (&item == &items[0] + selected);
+			glm::u8vec4 color = (is_selected ? item.selected_tint : item.tint);
+			float left, right;
+			if (!item.sprite) {
+				//draw item.name as text:
+				draw_sprites.draw_text(
+					item.name, item.at, item.scale, color
+				);
+				glm::vec2 min, max;
+				draw_sprites.get_text_extents(
+					item.name, item.at, item.scale, &min, &max
+				);
+				left = min.x;
+				right = max.x;
+			}
+			else {
+				draw_sprites.draw(*item.sprite, item.at, item.scale, color);
+				left = item.at.x + item.scale * (item.sprite->min_px.x - item.sprite->anchor_px.x);
+				right = item.at.x + item.scale * (item.sprite->max_px.x - item.sprite->anchor_px.x);
+			}
+			if (is_selected) {
+				if (left_select) {
+					draw_sprites.draw(*left_select, glm::vec2(left - bounce, item.at.y) + left_select_offset, item.scale, left_select_tint);
+				}
+				if (right_select) {
+					draw_sprites.draw(*right_select, glm::vec2(right + bounce, item.at.y) + right_select_offset, item.scale, right_select_tint);
+				}
+			}
+
+		}
+	} //<-- gets drawn here!
+
+
+	GL_ERRORS(); //PARANOIA: print errors just in case we did something wrong.
+}
+
+
+void MenuMode::layout_items(float gap) {
+	DrawSprites temp(*atlas, view_min, view_max, view_max - view_min, DrawSprites::AlignPixelPerfect); //<-- doesn't actually draw
+	float y = view_max.y;
+	for (auto& item : items) {
+		glm::vec2 min, max;
+		if (item.sprite) {
+			min = item.scale * (item.sprite->min_px - item.sprite->anchor_px);
+			max = item.scale * (item.sprite->max_px - item.sprite->anchor_px);
+		}
+		else {
+			temp.get_text_extents(item.name, glm::vec2(0.0f), item.scale, &min, &max);
+		}
+		item.at.y = y - max.y;
+		item.at.x = 0.5f * (view_max.x + view_min.x) - 0.5f * (max.x + min.x);
+		y = y - (max.y - min.y) - gap;
+	}
+	float ofs = -0.5f * y;
+	for (auto& item : items) {
+		item.at.y += ofs;
+	}
+}
