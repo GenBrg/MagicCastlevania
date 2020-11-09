d10919b21f5f8eb4fed9e78dee57bcd3ce100cd6
Snoopy <2322168080@qq.com>
2020-11-08 22:28:50 -0500

    Fix build error on linux and MacOS

--- engine/Animation.hpp
+++ engine/Animation.hpp
@@ -1,38 +1,3 @@
-// #pragma once
-
-// #include <vector>
-// #include <Sprite.hpp>
-// #include "Load.hpp"
-
-// inline std::unordered_map<std::string, std::vector<Sprite>> animationCollection;
-
-// Load< void > load_animations(LoadTagDefault, []() {
-// // init animationCollection here
-// });
-
-// class Animation {
-// private:
-// 	// points to a vector in the animation collection
-// 	std::vector<Sprite>* animationP;
-// 	// delay time of a single sprite
-// 	float delay;
-// 	// whether replay from start after playing the animation once
-// 	bool loopPlay;
-// 	int curSpriteIdx;
-// 	float elapsedSinceStart;
-
-// public:
-// 	Sprite getCurSprite() const;
-
-// 	void update(float elapsed);
-
-// 	void start();
-// 	void pause();
-
-// 	// return true when loopPlay == false && curSpriteIdx == animationP->size() - 1
-// 	bool complete();
-// };
-
 #pragma once
 
 #include "../Sprite.hpp"
@@ -43,6 +8,7 @@
 #include <unordered_map>
 #include <string>
 #include <algorithm>
+#include <iostream>
 
 using Animation = std::vector<const Sprite*>;
 // TODO change lookup system from string look up to template based enum look up.
