3d73c4a436209b185157f55ebc603abfd0523fd5
Chipxiang <jianxiangli1996@gmail.com>
2020-11-17 00:05:07 -0500

    Made menu much better.

--- main_menu.cpp
+++ main_menu.cpp
@@ -8,23 +8,19 @@ std::shared_ptr< MenuMode > main_menu;
 
 Load< void > load_main_menu(LoadTagLate, []() {
 	std::vector< MenuMode::Item > items;
-	items.emplace_back("[[ DEMO MENU ]]" ,&sprites->lookup("text_button_1"),&sprites->lookup("text_button_1_selected"));
-
-	items.emplace_back("Play", &sprites->lookup("text_button_1"), &sprites->lookup("text_button_1_selected"));
+	items.emplace_back("Static" ,&sprites->lookup("menu_static"),&sprites->lookup("menu_static"));
+	items.emplace_back("Start", &sprites->lookup("start_button"), &sprites->lookup("start_button_selected"));
 	items.back().on_select = [](MenuMode::Item const&) {
 		main_play = std::make_shared< PlayMode >();
 		Mode::set_current(main_play);
 	};
+	items.back().transform.position_ = glm::vec2(396.0f + 83.0f, 541.0f - (358.0f + 18.0f));
 
-	items.emplace_back("Play 2", &sprites->lookup("text_button_1"), &sprites->lookup("text_button_1_selected"));
+	items.emplace_back("Quit", &sprites->lookup("quit_button"), &sprites->lookup("quit_button_selected"));
 	items.back().on_select = [](MenuMode::Item const&) {
-		Mode::set_current(main_play);
+		exit(0);
 	};
-
-	items.emplace_back("Play 3", &sprites->lookup("text_button_1"), &sprites->lookup("text_button_1_selected"));
-	items.back().on_select = [](MenuMode::Item const&) {
-		Mode::set_current(main_play);
-	};
+	items.back().transform.position_ = glm::vec2(396.0f + 83.0f, 541.0f - (417.0f + 18.0f));
 
 	main_menu = std::make_shared< MenuMode >(items);
 	main_menu->selected = 1;
@@ -32,5 +28,4 @@ Load< void > load_main_menu(LoadTagLate, []() {
 	main_menu->view_min = glm::vec2(0.0f, 0.0f);
 	main_menu->view_max = glm::vec2(960.0f, 541.0f);
 
-	main_menu->vertical_layout_items(2.0f);
 	});
