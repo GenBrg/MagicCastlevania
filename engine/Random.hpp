59de378efec74794460e77b1a87e60e6303d291a
Snoopy <2322168080@qq.com>
2020-11-22 16:16:08 -0500

    Add random number generator

---
+++ engine/Random.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <engine/Singleton.hpp>
+
+#include <random>
+
+class Random : public Singleton<Random> {
+public:
+	/**
+	 * Generate a random floating point number between [0.0, 1.0)
+	 */
+	float Generate()
+	{
+		return dist(mt);
+	}
+
+private:
+	Random() {}
+
+	std::random_device rd {};
+	std::mt19937 mt {rd()};
+	std::uniform_real_distribution<float> dist { 0.0f, 1.0f };
+};
