9d76a6a8bceee6fbb562278cd24a3c2b7c7cb428
Chipxiang <jianxiangli1996@gmail.com>
2020-12-11 22:41:35 -0500

    Fixed 0 hp hud bug.

--- gamebase/HeadsUpDisplay.cpp
+++ gamebase/HeadsUpDisplay.cpp
@@ -45,22 +45,21 @@ hp_bar_transform_(nullptr)
 void HeadsUpDisplay::Draw(DrawSprites& draw_sprite) const {
 	draw_sprite.draw(sprites->lookup("hp_bar"), hp_bar_transform_);
 	int hp_to_draw = player->GetHp() * 100 / player->GetMaxHP();
-	if (hp_to_draw <= 0) {
-		return;
+	if (hp_to_draw > 0) {
+        Transform2D hp_curr_transform = Transform2D(nullptr);
+        hp_curr_transform.position_ = hp_bar_transform_.position_ + glm::vec2(3.0f, 4.0f);
+        draw_sprite.draw(sprites->lookup("hp_corner1"), hp_curr_transform);
+        hp_curr_transform.position_ += glm::vec2(4.0f, 0.0f);
+        hp_to_draw -= 3;
+        if (hp_to_draw >= 3) {
+            for (; hp_to_draw >= 3; hp_to_draw--) {
+                draw_sprite.draw(sprites->lookup("hp_point"), hp_curr_transform);
+                hp_curr_transform.position_ += glm::vec2(1.0f, 0.0f);
+            }
+            draw_sprite.draw(sprites->lookup("hp_corner2"), hp_curr_transform);
+        }
 	}
-	Transform2D hp_curr_transform = Transform2D(nullptr);
-	hp_curr_transform.position_ = hp_bar_transform_.position_ + glm::vec2(3.0f,4.0f);
-	draw_sprite.draw(sprites->lookup("hp_corner1"), hp_curr_transform);
-	hp_curr_transform.position_ += glm::vec2(4.0f, 0.0f);
-	hp_to_draw -= 3;
-	if (hp_to_draw < 3) {
-		return;
-	}
-	for (; hp_to_draw >= 3; hp_to_draw--) {
-		draw_sprite.draw(sprites->lookup("hp_point"), hp_curr_transform);
-		hp_curr_transform.position_ += glm::vec2(1.0f, 0.0f);
-	}
-	draw_sprite.draw(sprites->lookup("hp_corner2"), hp_curr_transform);
+	
 
 	auto skill_transform = Transform2D(nullptr);
 	skill_transform.position_ = hp_bar_transform_.position_ + glm::vec2(-38.0f, -40.0f);
